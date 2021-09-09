6bf0f95bae769fa2661c99038df81be47b28368a
henokwel <henokwel@outlook.com>
2021-09-09 11:07:34 +0200

    miner style changes and finishing up card style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, FormEvent, useState } from "react";
 import RemoveIcon from "./Assets/Icons/deleteIcon.svg";
-import { createAvatar } from '@dicebear/avatars';
-import * as style from '@dicebear/avatars-bottts-sprites';
+import { createAvatar } from "@dicebear/avatars";
+import * as style from "@dicebear/avatars-bottts-sprites";
 import "./App.css";
-
+import { time } from "console";
 
 interface Data {
   id: number;
@@ -19,14 +19,27 @@ interface Data {
 // https://avatars.dicebear.com/
 
 const App: React.FC = () => {
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<Data[]>([
+    {
+      id: 1,
+      title: "Testing",
+      description: "Making apps ",
+      done: false,
+      date: new Date(),
+    },
+    {
+      id: 2,
+      title: "Testing 2",
+      description: "Making apps for this company called spotify ",
+      done: false,
+      date: new Date(),
+    },
+  ]);
   const [editTitle, setEditTitle] = useState<string>("");
   // const [editTitle,setEditTitle] = useState<string>("")
   const titleRef = useRef<HTMLInputElement>(null);
   const descRef = useRef<HTMLTextAreaElement>(null);
 
- 
-  
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
     console.log(titleRef.current!.value);
@@ -53,18 +66,75 @@ const App: React.FC = () => {
         <h3>Let's work!</h3>
       </header>
       <main>
-        <section className="card">
-          <div className="card_header">
+        {data.map((item) => {
+          return (
+            <section className="card">
+              <div className="card_header">
+                <div className="card_avatar">
+                  {/* <img src={svg} alt="" width={50} height={50} /> */}
+                  <img
+                    src={`https://avatars.dicebear.com/api/bottts/:${item.title}.svg`}
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                </div>
+                <div className="card_timestamp">
+                  <small>
+                    <time dateTime="2008-02-14 20:00">
+                      {item.date.toDateString()}
+                    </time>
+                  </small>
+                </div>
+              </div>
+
+              <div className="card_body">
+                <form>
+                  <div className="card_content">
+                    <input
+                      type="text"
+                      value={item.title}
+                      aria-label="title"
+                      placeholder="Title"
+                    />
+                    <textarea
+                      value={item.description}
+                      aria-label="description of task"
+                      placeholder="Description"
+                    />
+                  </div>
+
+                  <div className="card_controll">
+                    <button>
+                      <img src={RemoveIcon} alt="" width={15} height={15} />
+                    </button>
+                    <input type="checkbox" checked={item.done} />
+                  </div>
+                </form>
+              </div>
+            </section>
+          );
+        })}
+
+        <section className="card newCard">
+          {/* <div className="card_header">
             <div className="card_avatar">
-              {/* <img src={svg} alt="" width={50} height={50} /> */}
-                <img src={`https://avatars.dicebear.com/api/bottts/:${"title"}.svg`} alt="" width={30} height={30} />
+               <img
+                src={`https://avatars.dicebear.com/api/bottts/:${item.title}.svg`}
+                alt=""
+                width={30}
+                height={30}
+              />
             </div>
             <div className="card_timestamp">
-              <small><time dateTime="2008-02-14 20:00">date</time></small>
+              <small>
+                <time dateTime="2008-02-14 20:00">
+                  {item.date.toDateString()}
+                </time>
+              </small>
             </div>
-          </div>
+          </div> */}
 
-          
           <div className="card_body">
             <form>
               <div className="card_content">
@@ -75,18 +145,18 @@ const App: React.FC = () => {
                   placeholder="Title"
                 />
                 <textarea
-                  value="Description goes here and it  f d to be smaller font"
+                  value="Descript"
                   aria-label="description of task"
                   placeholder="Description"
                 />
               </div>
 
-              <div className="card_controll">
+              {/* <div className="card_controll">
                 <button>
                   <img src={RemoveIcon} alt="" width={15} height={15} />
                 </button>
-                <input type="checkbox" />
-              </div>
+                <input type="checkbox" checked={item.done} />
+              </div> */}
             </form>
           </div>
         </section>
